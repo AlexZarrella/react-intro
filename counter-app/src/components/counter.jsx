d58dba96d1a24b336bf763610c35a80192a70b6b
alexzarrella <alexzarrella@Alexs-MacBook-Pro-3.local>
2018-08-20 11:42:58 -0400

    inline styling

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,15 +3,24 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0
+    // imageUrl: "https://picsum.photos/200"
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
