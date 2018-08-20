abccc60ab9e05cc8e770abc5c76c3c52b36b5559
alexzarrella <alexzarrella@Alexs-MacBook-Pro-3.local>
2018-08-20 11:08:39 -0400

    rewriting src files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const element = <h1>Hello World</h1>;
+
+React.createElement;
+console.log(element);
