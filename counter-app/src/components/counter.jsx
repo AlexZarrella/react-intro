9726cba869cd6117c94abb94a0f5b75cf0fc6f8a
alexzarrella <alexzarrella@Alexs-MacBook-Pro-3.local>
2018-08-20 12:49:21 -0400

    methods

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,19 +3,52 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0
+
     // imageUrl: "https://picsum.photos/200"
   };
 
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map(tag => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     );
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    this.state.count++;
+    this.setState();
+  };
+
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()} */}
       </div>
     );
+  }
+
+  //refactored into new method
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
