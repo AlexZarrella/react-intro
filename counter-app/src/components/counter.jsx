949c43a999ebca81f9e18ffe171f06603600f406
alexzarrella <alexzarrella@Alexs-MacBook-Pro-3.local>
2018-08-20 13:08:01 -0400

    increment methods

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -22,10 +22,14 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
 
-  handleIncrement = () => {
-    this.state.count++;
-    this.setState();
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
+
+  //   doHandleIncrement = () => {
+  //     this.handleIncrement({ id: 1 });
+  //   };
 
   render() {
     return (
@@ -33,7 +37,7 @@ class Counter extends Component {
         {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
